2a09f12ba36f4316433621e5a485af710f7df059
Martin Z Atanasov <martin.z.atanasov@gmail.com>
2023-11-25 16:33:09 +0200

    feat(gql): remove warehouse products queryies and mutations

--- packages/gql/src/warehouse-product/dto/update-warehouse-product.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateWarehouseProductInput } from './create-warehouse-product.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateWarehouseProductInput extends PartialType(CreateWarehouseProductInput) {
-  @Field(() => Int)
-  id: number;
-}
