420373208bdb09b3d4552f2e94365646cbdd99f3
Martin Z Atanasov <martin.z.atanasov@gmail.com>
2023-11-25 09:05:27 +0200

    feat(gql): handle cleanup on product remove

--- packages/gql/src/product/product.module.ts
+++ packages/gql/src/product/product.module.ts
@@ -3,9 +3,10 @@ import { ProductService } from './product.service';
 import { ProductResolver } from './product.resolver';
 import { Product } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CalculationsService } from 'src/shared/calculations.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
-  providers: [ProductResolver, ProductService],
+  providers: [ProductResolver, ProductService, CalculationsService],
 })
 export class ProductModule {}
--- packages/gql/src/product/product.service.ts
+++ packages/gql/src/product/product.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
 import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ProductsFilterInput } from './dto/products-filter-input';
+import { WarehouseProduct } from 'src/warehouse-product/entities/warehouse-product.entity';
+import { CalculationsService } from 'src/shared/calculations.service';
+import { Warehouse } from 'src/warehouse/entities/warehouse.entity';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
     private productRepository: Repository<Product>,
+    private calculationsService: CalculationsService,
   ) {}
 
   create(createProductInput: CreateProductInput) {
@@ -27,6 +31,7 @@ export class ProductService {
   }
 
   async update(updateProductInput: UpdateProductInput) {
+    // TODO: Handle clean up
     const product = await this.productRepository.findOneByOrFail({
       id: updateProductInput.id,
     });
@@ -37,7 +42,58 @@ export class ProductService {
     const product = await this.productRepository.findOneOrFail({
       where: { id },
     });
-    await this.productRepository.delete({ id });
-    return product;
+    return this.productRepository.manager.transaction(async (manager) => {
+      const productRepository = manager.getRepository(Product);
+      const warehouseRepository = manager.getRepository(Warehouse);
+      const warehouseProductRepository =
+        manager.getRepository(WarehouseProduct);
+
+      const warehouseProducts = await warehouseProductRepository.find({
+        where: { product: { id: id } },
+        relations: { warehouse: true },
+      });
+
+      const promisesResults = await Promise.allSettled(
+        warehouseProducts.map(async (warehouseProduct) => {
+          const warehouse = warehouseProduct.warehouse;
+
+          const productSize = await this.calculationsService.calculateItemsSize(
+            {
+              items: [
+                {
+                  quantity: warehouseProduct.quantity,
+                  sizePerUnit: product.size,
+                },
+              ],
+            },
+          );
+
+          await warehouseProductRepository.delete({ id: warehouseProduct.id });
+
+          if (productSize == warehouse.takenSize) {
+            warehouse.hazardous = false;
+            warehouse.takenSize = 0;
+            warehouse.availableSize = warehouse.size;
+          } else {
+            warehouse.takenSize = await this.calculationsService.deduct(
+              warehouse.takenSize,
+              productSize,
+            );
+            warehouse.availableSize = await this.calculationsService.sum([
+              warehouse.availableSize,
+              productSize,
+            ]);
+          }
+
+          await warehouseRepository.save(warehouse);
+        }),
+      );
+
+      const err = promisesResults.find((v) => v.status == 'rejected');
+      if (err) throw new InternalServerErrorException(err);
+
+      await productRepository.delete({ id });
+      return product;
+    });
   }
 }
