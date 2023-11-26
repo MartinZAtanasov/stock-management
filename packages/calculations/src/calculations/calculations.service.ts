515dddc5b19e6844fe82dd16ae4b5932ab5bb148
Martin Z Atanasov <martin.z.atanasov@gmail.com>
2023-11-26 09:37:25 +0200

    chore(calculations api): use mathjs for calculations

--- packages/calculations/src/calculations/calculations.service.ts
+++ packages/calculations/src/calculations/calculations.service.ts
@@ -2,23 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { CalculateItemsSizeInput } from './dto/calculate-items-size.input';
 import { SumInput } from './dto/sum.input';
 import { DeductInput } from './dto/deduct.input';
+import { subtract, add, multiply } from 'mathjs';
 
 @Injectable()
 export class CalculationsService {
   calculateItemsSize({ items }: CalculateItemsSizeInput) {
     const itemsSize = items.reduce((acc, current) => {
-      const itemSize = current.quantity * current.sizePerUnit;
-      return acc + itemSize;
+      const itemSize = multiply(current.quantity, current.sizePerUnit);
+      return add(acc, itemSize);
     }, 0);
 
     return { result: itemsSize };
   }
 
   async sum({ numbers }: SumInput) {
-    return { result: numbers.reduce((a, b) => a + b, 0) };
+    return { result: numbers.reduce((a, b) => add(a, b), 0) };
   }
 
   async deduct({ numberA, numberB }: DeductInput) {
-    return { result: numberA - numberB };
+    return { result: subtract(numberA, numberB) };
   }
 }
